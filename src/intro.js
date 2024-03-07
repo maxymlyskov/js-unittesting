23825d588defca9e616242678454448a8300e28c
maxymlyskov <raypember02@gmail.com>
2024-03-07 10:53:25 +0200

    Add calculateAverage using TDD.

--- src/intro.js
+++ src/intro.js
@@ -5,8 +5,15 @@ export function max(a, b) {
 
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n.toString();
+}
+
+export function calcualteAverage(numbers) {
+  if (numbers.length === 0) return NaN;
+  const sum = numbers.reduce((sum, curr) => sum + curr, 0);
+
+  return sum / numbers.length;
 }
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,29 +1,44 @@
-import { it, expect, describe } from 'vitest'
-import { max,fizzBuzz } from '../src/intro'
+import { it, expect, describe } from "vitest";
+import { max, fizzBuzz, calcualteAverage } from "../src/intro";
 
-describe('max', () => {
-    it('should return the first argument if it is greater', () => {
-        expect(max(2, 1)).toBe(2)
-    })
-    it('should return the second argument if it is greater', () => {
-        expect(max(1, 2)).toBe(2)
-    })
-    it('should return the first argument if arguments are equal', () => {
-        expect(max(2, 2)).toBe(2)
-    })
-})
+describe("max", () => {
+  it("should return the first argument if it is greater", () => {
+    expect(max(2, 1)).toBe(2);
+  });
+  it("should return the second argument if it is greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+  it("should return the first argument if arguments are equal", () => {
+    expect(max(2, 2)).toBe(2);
+  });
+});
 
-describe('fizzBuzz', () => {
-    it('should return FizzBuzz if n is multiple of 3 and 5', () => {
-        expect(fizzBuzz(15)).toBe('FizzBuzz')
-    })
-    it('should return Fizz if n is multiple of 3', () => {
-        expect(fizzBuzz(9)).toBe('Fizz')
-    })
-    it('should return Buzz if n is multiple of 5', () => {
-        expect(fizzBuzz(10)).toBe('Buzz')
-    })
-    it('should return arg as a string if n is not multiple of 5 or 3', () => {
-        expect(fizzBuzz(1)).toBe('1')
-    })
-})
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if n is multiple of 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return Fizz if n is multiple of 3", () => {
+    expect(fizzBuzz(9)).toBe("Fizz");
+  });
+  it("should return Buzz if n is multiple of 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+  it("should return arg as a string if n is not multiple of 5 or 3", () => {
+    expect(fizzBuzz(1)).toBe("1");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("should return NaN if array is empty", () => {
+    expect(calcualteAverage([])).toBe(NaN);
+  });
+  it("should calculate the average of an array with a single element", () => {
+    expect(calcualteAverage([1])).toBe(1);
+  });
+  it("should calculate the average of an array with two elements", () => {
+    expect(calcualteAverage([1, 3])).toBe(2);
+  });
+  it("should calculate the average of an array with three elements", () => {
+    expect(calcualteAverage([1, 2, 3])).toBe(2);
+  });
+});
