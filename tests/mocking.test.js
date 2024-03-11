e13f06349682b6febd4206b84ab5fcad7d40c51e
maxymlyskov <raypember02@gmail.com>
2024-03-11 12:42:34 +0200

    Add getPriceInCurrency test.

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,8 @@
 import { it, expect, describe, vi } from "vitest";
+import { getPriceInCurrency } from "../src/mocking";
+import { getExchangeRate } from "../src/libs/currency";
+
+vi.mock("../src/libs/currency");
 
 describe("test suite", () => {
   it("sendText", () => {
@@ -9,5 +13,15 @@ describe("test suite", () => {
 
     expect(sendText).toHaveBeenCalledWith("message");
     expect(sendText).toHaveReturnedWith("Ok");
+  });
+});
+
+describe("getPriceInCurrency", () => {
+  it("should return price in current currency", () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+    const price = getPriceInCurrency(10, "AUD");
+
+    expect(price).toBe(15);
   });
 });
